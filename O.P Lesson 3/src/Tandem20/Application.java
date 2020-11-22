c5d12467e21542d06a6ea90488f39c98ce79cebd
ASUS <ASUS@Tetyana>
2020-11-22 12:03:19 +0200

    functions,strings,switch

---
+++ O.P Lesson 3/src/Tandem20/Application.java
@@ -0,0 +1,26 @@
+package Tandem20;
+
+public class Application {
+	public static void main(String[] args) {
+		String day1 = "Понеділок";
+		String day2 = "Вівторок";
+		String day3 = "Середа";
+		String day4 = "Четверг";
+		String day5 = "П'ятниця";
+		String day6 = "Субота";
+		String day7 = "Неділя";
+		System.out.println(day1 + day2 + day3 + day4 + day5 + day6 + day7);
+		
+	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
+
---
+++ O.P Lesson 3/src/Tandem20/Application1.java
@@ -0,0 +1,44 @@
+package Tandem20;
+
+public class Application1 {
+	public static void main(String[] args) {
+		String a = "Четверг";
+		switch (a ) {
+			case "Вівторок" : {
+				System.out.println(" Навчання");
+				break;
+			}
+			case "Понеділок" : {
+				System.out.println(" Робота");
+				break;
+			}
+			case "Середа" : {
+				System.out.println("Фітнес");
+				break;
+			}
+			case "Четверг" : {
+				System.out.println(" Відрядження");
+				break;
+			}
+			case "П'ятниця" : {
+				System.out.println(" Здати звіт");
+				break;
+			}
+			case "Субота" : {
+				System.out.println(" Відвідати батьків");
+				break;
+			}
+			case "Неділя" : {
+				System.out.println(" Відпочивати");
+				break;
+			}
+			default:
+				System.out.println("default");
+			break;
+		}
+		
+		
+	}	
+		
+	}
+
